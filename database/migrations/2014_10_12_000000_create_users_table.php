e219061c3d68d054c14387b6543f9653a4403c11
at-dungvan <dungvan2512@gmail.com>
2017-07-27 07:40:52 +0000

    update migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('full_name');
             $table->string('email')->unique();
-            $table->string('password');
+            $table->date('birthday');
+            $table->boolean('gender');
+            $table->string('address');
+            $table->string('phone_number', 15);
+            $table->string('image');
+            $table->boolean('is_admin');
+            $table->boolean('is_active');
             $table->rememberToken();
             $table->timestamps();
         });
